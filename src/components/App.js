a2c44189f512ec5fdfc1c20d7b6518dcbfdb5e3a
Nanditha Embar <nanditha.embar@sentry.io>
2021-07-21 07:59:48 -0700

    updated app.js

--- src/components/App.js
+++ src/components/App.js
@@ -100,8 +100,8 @@ class App extends Component {
       level: 'info'
     });
   
-    this.codeIsNotPerfect();
-    // this.isaDummyError();
+    // this.codeIsNotPerfect();
+    this.isaDummyError();
     
     if (WORKFLOW) {
       this.myCodeIsNotPerfect();
