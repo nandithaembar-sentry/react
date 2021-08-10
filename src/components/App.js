d9863961f149f421403e7151ff98cf19bba0483f
Nanditha Embar <nanditha.embar@sentry.io>
2021-08-10 15:58:29 -0700

    Changes made to app.js

--- src/components/App.js
+++ src/components/App.js
@@ -102,7 +102,7 @@ class App extends Component {
   
   //this is the update
     // this.isanError();
-    this.isaSampleError();
+    this.thisisanError();
     
     if (WORKFLOW) {
       this.myCodeIsNotPerfect();
